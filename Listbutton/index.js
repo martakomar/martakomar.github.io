2dfd30703b5d6743c5fa80dd09e5544b731b36e5
Marta Komar <marta.komar@bellevuecollege.edu>
2018-10-10 12:24:21 -0700

    list button

---
+++ Listbutton/index.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    const userInput = document.getElementById('list-data');    
+    const setTextButton = document.getElementById('set-text');
+    const listButton = document.getElementById('add-to-list');
+    
+    setTextButton.onclick = () => {
+        userInput.value = 'The first item for my list';
+    }
+}    
