dfeafdd35a32dedb94e99dae0bf051a5ba70aab6
Marta Komar <marta.komar@bellevuecollege.edu>
2018-10-31 11:48:51 -0700

    Wednesdays work

---
+++ javascript-test/week07-day02.js
@@ -0,0 +1,33 @@
+var showMessage = (message) => {
+console.log("=====================");
+console.log("== " + message);
+console.log("======================");
+}
+
+showMessage('Operator Precedence');
+
+let result = 1 + 2 * 3;
+console.log(result);
+
+result = (1 + 2) * 3;
+console.log(result);
+
+showMessage('For Loops');
+
+for (let i = 10; i <= 25; i = i + 2) {
+    console.log(i);
+}
+
+showMessage('While Loops');
+
+const looper = 10;
+
+while (looper < 15) {
+    console.log(looper++);
+}
+
+do {
+    console.log(looper++);
+} while (looper < 20);
+
+
