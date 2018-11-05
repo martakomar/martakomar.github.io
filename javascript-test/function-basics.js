d46433e6bd7cdbc1a2f75fba108b7684f440e083
Marta Komar <marta.komar@bellevuecollege.edu>
2018-11-05 12:17:32 -0800

    function basics

---
+++ javascript-test/function-basics.js
@@ -0,0 +1,20 @@
+function simpleFunction() {
+    console.log("functions should consist of statements designed to perform a single task.");
+}
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+function adder () {
+    console.log(2 + 3);
+}
+
+function functionParameters(value) {
+    console.log(value)
+}
+
+simpleFunction();
+functionParameters("Many functions take parameters.")
+add(2, 3);
+adder();
