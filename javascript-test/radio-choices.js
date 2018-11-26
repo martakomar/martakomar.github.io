046ed549b322ab749eb945dc97cf3da8ad692797
Marta Komar <marta.komar@bellevuecollege.edu>
2018-11-26 11:33:36 -0800

    Mondays work

---
+++ javascript-test/radio-choices.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    const myForm = document.getElementById('sort-type');
+
+    myForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const value = document.querySelector('input[name="sorter"]:checked').value;
+        if (value.toLowerCase() === 'language') {
+            showChoice(value);
+        } else {
+            showChoice(value);
+        }
+    });
+}
